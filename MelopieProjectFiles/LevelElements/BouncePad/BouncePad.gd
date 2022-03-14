41e9284649945d779fd608b56e93b05c73a31328
Corentin <corentin.derette@gmail.com>
2022-03-14 20:31:02 +0100

    start add bounce

---
+++ MelopieProjectFiles/LevelElements/BouncePad/BouncePad.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+export var bounce_strength: float
+export var bounce_direction: Vector2
+
+func _ready():
+	connect("body_entered", self, "on_body_entered")
+	
+func on_body_entered(body: Node2D):
+	bounce(body)
+
